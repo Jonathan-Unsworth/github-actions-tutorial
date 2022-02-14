c58b90ff1ec863b199f9fcd2e6ceb702913c89d1
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2022-02-14 15:18:03 +0000

    added unit tests

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import arithmethic
+
+
+def main():
+    # Test
+    print(arithmethic.add(1, 3))
+    print(arithmethic.mul(2, 5))
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/test_arithmethic.py
@@ -0,0 +1,9 @@
+import arithmethic
+
+
+def test_add():
+    assert arithmethic.add(2, 2) == 4
+
+
+def test_mul():
+    assert arithmethic.mul(2, 5) == 10
